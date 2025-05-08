d85a9147fa6e70fc64e5681431a5781655ce064f
Arvelmay <arvelmay.github@gmail.com>
2025-05-08 13:12:28 +0000

    Start Audio

---
+++ src/bogus/audio/AudioLoader.java
@@ -0,0 +1,14 @@
+package bogus.audio;
+
+import bogus.core.*;
+import bogus.util.*;
+
+public class AudioLoader extends Loader {
+    Logger logger = new Logger(loaderName);
+
+    @Override
+    public void load() {
+        // TODO Auto-generated method stub
+        logger.log("Not implemented yet");
+    }
+}
