ca5c1ee60521092111239b07ed0a09cb0c1163f9
Arvelmay <arvelmay.github@gmail.com>
2025-05-09 14:42:38 +0000

    Making Drawer

---
+++ src/bogus/graphics/Drawer.java
@@ -0,0 +1,14 @@
+package bogus.graphics;
+
+import bogus.assets.*;
+
+import java.awt.*;
+
+public class Drawer {
+    
+    public void draw(int x, int y, ImageAsset asset){
+        
+        Window.add();
+    }
+
+}
--- src/bogus/graphics/Window.java
+++ src/bogus/graphics/Window.java
@@ -1,11 +1,15 @@
 package bogus.graphics;
 
+import bogus.util.*;
+
 import java.awt.*;
 
 public class Window extends Frame {
+    static Logger logger = new Logger("loaderName");
     
     public Window(String tital) {
         super(tital);
+        logger.log("Making new Window");
     }
 
 }
